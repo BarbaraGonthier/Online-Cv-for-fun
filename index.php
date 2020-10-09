0a690ce3d3b68c75c9206e90a9740c71fcdfa7ea
Barbara Gonthier <barbara.gonthier@gmail.com>
2020-10-09 09:30:12 +0200

    header.php added

--- index.php
+++ index.php
@@ -12,36 +12,7 @@ include ('data.php');
 </head>
 <body>
     <header>
-        <nav>
-            <div class="menu-items">
-                <div class="desktop-menu" id="menu">
-                    <a href="#home" title="Accueil">Accueil</a>
-                    <a href="#experience" title="Expérience">Expérience</a>
-                    <a href="#education" title="Formation">Formation</a>
-                    <a href="#skills" title="Compétences">Atouts</a>
-                    <a href="#interests" title="Centres d'intérêts">Centres d'intérêts</a>
-                    <a href="#contact" title="Contact">Contact</a>
-                    <a href="#home" class="close-burger" title="Fermer le menu">X</a> 
-                </div>
-                <a href="#menu" class="burger" title="Ouvrir le menu">
-                    <div></div>
-                    <div></div>
-                    <div></div>   
-                </a>
-            </div>
-            <div class="download-cv">
-                <a href="CV-Bob-Roborovski.pdf"
-                target="_blank"
-                download="CV-Bob-Roborovski.pdf"
-                title="Télécharger le CV en pdf">Télécharger le CV en pdf</a>
-            </div>
-            <div class="download-cv-mobile">
-                <a href="CV-Bob-Roborovski.pdf"
-                target="_blank"
-                download="CV-Bob-Roborovski.pdf"
-                title="Télécharger le CV en pdf">Pdf</a>
-            </div>
-        </nav>
+        <?php include 'header.php' ?>
     </header>
     <section id="home">
         <div class="ball-left">
